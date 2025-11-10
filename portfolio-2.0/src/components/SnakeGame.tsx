ba841e71144b846bfdb16e4bc4488b74dbbabc4d
Luiz Laikovski <luizlaikovski@gmail.com>
2025-11-10 10:19:23 -0300

    8

--- portfolio-2.0/src/components/SnakeGame.tsx
+++ portfolio-2.0/src/components/SnakeGame.tsx
@@ -9,7 +9,7 @@ export default function SnakeGame() {
   const gridSize = 20;
   const cellSize = 25;
   const canvasSize = gridSize * cellSize;
-  const moveSpeed = 6; // velocidade suave
+  const moveSpeed = 8; // velocidade suave
 
   const stateRef = useRef({
     snake: [
